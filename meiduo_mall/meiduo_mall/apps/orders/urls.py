38ac230ec57d7634bfe7488b7bb3de441fee3849
huzing2524 <hu200901959>
2018-08-22 21:58:25 +0800

    OrderList commit

--- meiduo_mall/meiduo_mall/apps/orders/urls.py
+++ meiduo_mall/meiduo_mall/apps/orders/urls.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
+from django.conf.urls import url
 
+from orders import views
+
+urlpatterns = [
+    url(r"^orders/settlement/$", views.OrderSettlementView.as_view()),
+    url(r"^orders/$", views.SaveOrderView.as_view())
+]
--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),  # 富文本编辑器
     url(r"", include("goods.urls")),
     url(r"", include("carts.urls")),
-
+    url(r"", include("orders.urls")),
 ]
