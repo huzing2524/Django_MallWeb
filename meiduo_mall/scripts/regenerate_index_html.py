b91e05a3f9eb674f84554aad6960cd7c03d034e2
huzing2524 <826956010@qq.com>
2020-07-16 15:33:10 +0800

    fix scripts中生成静态主页html的导包先后顺序错误

--- meiduo_mall/scripts/regenerate_index_html.py
+++ meiduo_mall/scripts/regenerate_index_html.py
@@ -3,8 +3,6 @@ import sys
 import os
 import django
 
-from contents.crons import generate_static_index_html
-
 sys.path.insert(0, "../")
 
 if not os.getenv("DJANGO_SETTINGS_MODULE"):
@@ -12,6 +10,8 @@ if not os.getenv("DJANGO_SETTINGS_MODULE"):
 
 # django初始化，加载django功能
 django.setup()
+
+from contents.crons import generate_static_index_html
 
 if __name__ == '__main__':
     generate_static_index_html()
