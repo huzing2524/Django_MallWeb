39c3697786fe0a5bf29dda3f827fb81219f4d591
huzing2524 <826956010@qq.com>
2020-07-27 23:46:13 +0800

    fix 商品xadmin管理器类中对应关系错误

--- meiduo_mall/meiduo_mall/apps/goods/adminx.py
+++ meiduo_mall/meiduo_mall/apps/goods/adminx.py
@@ -50,6 +50,30 @@ class SKUSpecificationAdmin(object):
         generate_static_sku_detail_html.delay(sku_id)
 
 
+class SKUImageAdmin(object):
+    def save_models(self):
+        obj = self.new_obj
+        obj.save()
+
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(obj.sku.id)
+
+        # 设置SKU默认图片
+        sku = obj.sku
+        if not sku.default_image_url:
+            # http://image.meiduo.site:8888/groupxxxxxx
+            sku.default_image_url = obj.image.url
+            sku.save()
+
+    def delete_model(self):
+        obj = self.obj
+        sku_id = obj.sku.id
+        obj.delete()
+
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(sku_id)
+
+
 xadmin.site.register(views.BaseAdminView, BaseSetting)
 xadmin.site.register(views.CommAdminView, GlobalSettings)
 
@@ -58,7 +82,7 @@ xadmin.site.register(models.GoodsChannel)
 xadmin.site.register(models.Goods)
 xadmin.site.register(models.Brand)
 xadmin.site.register(models.GoodsSpecification)
-xadmin.site.register(models.SpecificationOption, SKUSpecificationAdmin)
+xadmin.site.register(models.SpecificationOption)
 xadmin.site.register(models.SKU, SKUAdmin)
-xadmin.site.register(models.SKUSpecification)
+xadmin.site.register(models.SKUSpecification, SKUSpecificationAdmin)
 xadmin.site.register(models.SKUImage)
